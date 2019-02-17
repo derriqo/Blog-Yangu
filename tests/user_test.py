2016e29353b52c2408994711633d6f035a564516
derrrick william <williamderrick100@gmail.com>
2019-02-17 19:25:41 +0300

    Finished Testing for User.

--- tests/user_test.py
+++ tests/user_test.py
@@ -1,12 +1,24 @@
 import unittest # Importing the unittest module
 from models import User # Importing the user class
 
-class TestUser(unittest.TestCase):
+class UserModeTest(unittest.TestCase):
 
     '''
     Test class that defines test cases for the user class behaviours.
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
+    '''
 
-    
+    def setUp(self):
+        self.new_user = User(password = 'banana')
+
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.pass_secure is not None)
+
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+        def test_password_verification(self):
+            self.assertTrue(self.new_user.verify_password('banana'))
