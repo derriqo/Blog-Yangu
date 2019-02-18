dc547a38189de58ff4890812ff316051b5007bb8
derrrick william <williamderrick100@gmail.com>
2019-02-18 05:05:25 +0300

    Created forms to be used .

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
 from flask import render_template
 from . import main
-from flask_login import login_required
+# from flask_login import login_required
 
 #Views 
-@main.route('/',methods = ['GET','POST'])
+@main.route('/')
 def index():
 
     '''
